7f1c7e738b77513966ffac56086e4ce7a92d7531
Simon Jefferies <simon.jefferies@inseinc.com>
2025-12-11 14:38:47 +0000

    Corrected namespace to `FluentLogger`.

--- CallerLogContextExtensions.cs
+++ CallerLogContextExtensions.cs
@@ -1,6 +1,6 @@
 using Microsoft.Extensions.Logging;
 
-namespace Inspired.CoreUI;
+namespace FluentLogger;
 
 public static class CallerLogContextExtensions
 {
--- FluentLoggerExtensions.cs
+++ FluentLoggerExtensions.cs
@@ -1,7 +1,7 @@
 using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Logging;
 
-namespace Inspired.CoreUI;
+namespace FluentLogger;
 
 public static class FluentLoggerExtensions
 {
